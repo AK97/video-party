c3a571cc36c735b41a509f6f5cb9e20e7d39b26e
AK97 <ashwinkumar97@hotmail.com>
2020-05-11 19:12:06 -0400

    time & play status appears to be synchronizing accross clients

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+var fs = require('fs');
 
+var Log = require('log');
+var log = new Log('info');
+
+var port = 69;
+
+var currentStatus = [0, true];
+
+app.use('/styles',express.static(__dirname + '/styles')); //provide client with (static) stylesheets
+
+io.on('connection', (socket) => {
+	console.log('user joined with ID ' + socket.id)
+	socket.emit('statusUpdate', currentStatus);
+	socket.on('actionPerform', (videoStatus) => {
+		currentStatus = videoStatus;
+		console.log('user ' + socket.id + ' changed video status to ' + videoStatus);
+		io.emit('statusUpdate', currentStatus);
+	});
+	socket.on('disconnect', () => {
+		console.log('user disconnected with id ' + socket.id);
+	})
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/video', (req, res) => { //upon request for /video...
+  var path = 'media/movie.mp4';
+  var stat = fs.statSync(path);
+  var fileSize = stat.size; //get the size of the video
+  var range = req.headers.range;
+  if (range) {
+  	var parts = range.replace(/bytes=/, "").split("-");
+  	var start = parseInt(parts[0], 10);
+  	var end = parts[1]
+  		? parseInt(parts[1], 10)
+  		: fileSize-1;
+  	var chunkSize = (end-start)+1;
+  	var file = fs.createReadStream(path, {start,end});
+	var head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunkSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(206, head);
+    file.pipe(res);
+  	}
+  	else { //starts here first time
+    	var head = {
+      	'Content-Length': fileSize,
+      	'Content-Type': 'video/mp4',
+    } //establish head
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+});
+
+http.listen(port, () => {
+  console.log('listening on *:'+port);
+});
