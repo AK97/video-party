7f90794d2a1c916e9ca8f92b8952c1a15a431a15
AK97 <ashwinkumar97@hotmail.com>
2020-05-18 00:08:46 -0400

    added party html and cleaned up chunk stream block

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//// Setup ////
+
 var express = require("express");
 var app = express();
 var http = require("http").createServer(app);
@@ -6,18 +8,59 @@ var fs = require('fs');
 
 var port = 69;
 
+//// Helper Functions ////
+
 function currentTime() { //returns current time in "[HH:MM] " 24hr format (string)
   var d = new Date();
   return '['+d.toTimeString().substr(0,5)+'] ';
 }
 
+//// Global Vars & Data ////
+
 var currentStatus = [0, true];
-
 var nicknames = {};
-
 var all_messages = []; //there needs to be some kind of room instantiation for this to make sense in the future
 
+//// Express Functions ////
+
 app.use('/styles',express.static(__dirname + '/styles')); //provide client with (static) stylesheets
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/party.html'); //send party.html when homepage is requested
+  //this will change to index html once index is built to be a room creating page
+});
+
+app.get('/video', (req, res) => { //upon request for /video...
+  var path = 'media/movie.mp4';
+  var fileSize = fs.statSync(path).size; //get the size of the video
+  var range = req.headers.range;
+  console.log(req.headers);
+  if (range) {
+    //the range property is as a string="bytes=0-" where 0 corresponds to which part of the video is being requested
+  	var parts = range.replace(/bytes=/, "").split("-"); //string of number that tells which byte to start loading at
+  	var start = parseInt(parts[0], 10); //convert string to int (base 10)
+  	var end = fileSize-1;
+  	var chunkSize = (end-start)+1;
+    var head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunkSize,
+        'Content-Type': 'video/mp4',
+      }
+    res.writeHead(206, head);
+    fs.createReadStream(path, {start,end}).pipe(res);
+  }
+  else { //starts here first time
+    var head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res) //open the video file as a readable stream then "pipe" it to the response object (which goes to the client)
+  }
+});
+
+//// Socket Functions ////
 
 io.on('connection', (socket) => {
 	console.log('user joined with ID ' + socket.id)
@@ -46,41 +89,7 @@ io.on('connection', (socket) => {
 	})
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
 
-app.get('/video', (req, res) => { //upon request for /video...
-  var path = 'media/movie.mp4';
-  var stat = fs.statSync(path);
-  var fileSize = stat.size; //get the size of the video
-  var range = req.headers.range;
-  if (range) {
-  	var parts = range.replace(/bytes=/, "").split("-");
-  	var start = parseInt(parts[0], 10);
-  	var end = parts[1]
-  		? parseInt(parts[1], 10)
-  		: fileSize-1;
-  	var chunkSize = (end-start)+1;
-  	var file = fs.createReadStream(path, {start,end});
-	var head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunkSize,
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(206, head);
-    file.pipe(res);
-  	}
-  	else { //starts here first time
-    	var head = {
-      	'Content-Length': fileSize,
-      	'Content-Type': 'video/mp4',
-    } //establish head
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
-  }
-});
 
 http.listen(port, () => {
   console.log('listening on *:'+port);
