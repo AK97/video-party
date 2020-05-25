b6c472451f425c73e78f9f5218fbeca8f3f87cef
AK97 <ashwinkumar97@hotmail.com>
2020-05-25 11:45:55 -0400

    v0.3: each room has file library. switched to mediaelement player. added favicon.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var fs = require('fs');
 var session = require('express-session');
 var siofu = require("socketio-file-upload");
 // var fileUpload = require('express-fileupload');
+var favicon = require('serve-favicon');
 
 
 const url = 'localhost:69/';
@@ -22,34 +23,62 @@ function currentTime() { //returns current time in "[HH:MM] " 24hr format (strin
 
 function generatePartyCode() {
 	return (
-		'vp' + Math.random().toString(36).substring(2, 15)
+		'vp' + Math.random().toString(36).substr(2, 6)
 	);
 }
 
+class QueueItem {
+    constructor(type, link, title, added_by) {
+        this.type = type;
+        this.link = link;
+        this.title = title;
+        this.added_by = added_by;
+        this.urlcode = generatePartyCode();
+        this.nowPlaying = false;
+
+        switch(type) {
+            case 'mp4 video':
+                this.typeLabel = 'video/mp4';
+                break;
+            case 'Youtube video':
+                this.typeLabel = 'youtube'; //TO BE CHANGED
+                break;
+            default:
+                this.typeLabel = '';
+                break;
+        }
+    }
+}
+
 class Party {
-    constructor(host, file) {
+    constructor(host) {
         this.host = host; //host's session ID
-        this.filepath = file; //filepath
+        this.filepath = '';
         this.code = generatePartyCode();
         this.members = {}; //store members by socketid:username because nicknames aren't important enough to track accross refreshes. allowing people to rename themself is okay.
-        this.currentStatus = [0,true];
-
         this.message_log = []; //complete message history
         this.message_log.push('Invite friends using the party code!');
         this.message_log.push('Party Code: ' + this.code);
+        this.currentStatus = [0,true];
+        this.queue = [];
+    }
+    playNow(filepath, nowPlaying) {
+        this.filepath = filepath;
+        this.nowPlaying = nowPlaying;
     }
 }
 
 //// Database ////
 
 var parties = {
-    'vptestroom1':new Party(),
-    'vptestroom2':new Party()
+    'vptestr1':new Party(),
+    'vptestr2':new Party()
 }; //contains all active rooms as partyid:Party
 
 //// Express Functions ////
 
 app.use('/styles',express.static(__dirname + '/styles')); //provide client with (static) stylesheets
+app.use(favicon(__dirname + '/images/favicon.ico')); //serve favicon
 
 // app.use(fileUpload());
 
@@ -86,19 +115,28 @@ app.use(siofu.router);
 
 // });
 
-app.get('/video*', (req, res) => { //upon request for /video(partyurl) -- this will be req's by a partyroom's video tag
-    let attempt = req.path.substr(6);
+app.get('/video*', (req, res) => { //upon request for /video(partycode)(videocode) -- this will be req'd by a partyroom's video tag
+    //forcing every video to have a unique url might prevent cookie issues in browser
+    let attempt = req.path.substr(6,8); //just partycode
+    let vidcode = req.path.substr(14); //just videocode
+    // for (var qi = 0; qi < parties[attempt].queue.length; qi++) {
+    //     if (parties[attempt].queue[qi].urlcode == vidcode) {
+    //         var path = parties[attempt].filepath;
+    //     }
+    // }
     var path = parties[attempt].filepath;
-    //var path = 'media/movie.mp4';
-    
+    // var path = 'media/movie.mp4';
     var fileSize = fs.statSync(path).size; //get the size of the video
     var range = req.headers.range;
     //console.log(req.headers);
     if (range) {
-        //the range property is as a string="bytes=0-" where 0 corresponds to which part of the video is being requested
+        //the range property is a string="bytes=0-" where 0 corresponds to which part of the video is being requested
         var parts = range.replace(/bytes=/, "").split("-"); //string of number that tells which byte to start loading at
         var start = parseInt(parts[0], 10); //convert string to int (base 10)
-        var end = fileSize-1; //in bytes; so could be like 1000000 to load 1mb at a time
+        //var end = fileSize-1; //in bytes; so could be like 1000000 to load 1mb at a time
+        const end = parts[1] 
+            ? parseInt(parts[1], 10)
+            : fileSize-1;
         var chunkSize = (end-start)+1;
         var head = {
             'Content-Range': `bytes ${start}-${end}/${fileSize}`,
@@ -132,25 +170,6 @@ io.use(function(socket, next) {
 indexsocket.on('connection', (socket) => {
     console.log('user reached homepage with session ID' + socket.request.session.id);
 
-    var uploader = new siofu();
-    uploader.dir = 'media';
-    uploader.listen(socket);
-
-    uploader.on("start", (event) => {
-        console.log('new file upload starting from user with socket id: ' + socket.id)
-    });
-    
-    uploader.on("saved", function(event){
-        //a file has been uploaded. we can start a party room now.
-        let party = new Party('unknown host', event.file.pathName);
-        parties[party.code] = party;
-        socket.emit('upload success', 'Upload complete! Your party code is: ' + party.code)
-    });
-
-    uploader.on("error", function(event){
-        console.log("Error from uploader", event);
-    });
-
     socket.on('join party', (code) => {
         //check if exists then instruct a redirect
         // console.log(parties);
@@ -163,14 +182,14 @@ indexsocket.on('connection', (socket) => {
         }
     });
 
-    socket.on('create party', (file) => {
+    socket.on('create party', () => {
         let host = socket.request.session.id;
-        let party = new Party(host, file);
+        let party = new Party(host);
         parties[party.code] = party;
-        socket.emit('go to party', url+party.code);
-        //if filesize is under limit and the format is mp4
+        socket.emit('go to party', party.code);
+        //check if the custom party code is already in use
         // else {
-        //     socket.emit('create error', 'Sorry, only mp4 files up to 5GB are allowed');
+        //     socket.emit('create error', 'Sorry, that party code is taken');
         // }
     });
 
@@ -190,18 +209,52 @@ partysocket.on('connection', (socket) => {
     if (parties[roomToConnect]) {
         parties[roomToConnect].members[socket.id] = "Anonymous";
         socket.emit('messagePopulate', parties[roomToConnect].message_log); //give joiner complete message history
-        socket.emit('statusUpdate', parties[roomToConnect].currentStatus); //catchup the joiner to position in video
+        partysocket.to(roomToConnect).emit('userPopulate', (Object.values(parties[roomToConnect].members))); //update everyone in the room's userlist
+        socket.emit('queue update', parties[roomToConnect].queue); //give joiner queue
+        if (parties[roomToConnect].nowPlaying) { //if there's something playing...
+            socket.emit('start playing', 'video'+roomToConnect+parties[roomToConnect].nowPlaying); //load up currently playing video
+            socket.emit('statusUpdate', parties[roomToConnect].currentStatus); //catchup the joiner to position in video
+        }
     }
-    socket.emit('setup video', roomToConnect);
     
+    var uploader = new siofu();
+    uploader.dir = 'media';
+    uploader.listen(socket);
+
+    uploader.on("start", (event) => {
+        console.log('new file upload starting from user with socket id: ' + socket.id + ' in party ' + roomToConnect)
+    });
+    
+    uploader.on("saved", function(event){
+        //a file has been uploaded. add it to queue
+        console.log(event)
+        parties[roomToConnect].queue.push(new QueueItem('mp4 video', event.file.pathName, event.file.name, parties[roomToConnect].members[socket.id]));
+        //refresh everyone's queues
+        partysocket.to(roomToConnect).emit('queue update', parties[roomToConnect].queue);
+    });
+
+    uploader.on("error", function(event){
+        console.log("Error from uploader", event);
+    });
+
+    socket.on('play request', (q) => {
+        console.log('player request:')
+        console.log(parties[roomToConnect].queue[q].typeLabel)
+        if (parties[roomToConnect].queue[q].typeLabel = 'video/mp4') {
+            parties[roomToConnect].playNow(parties[roomToConnect].queue[q].link, parties[roomToConnect].queue[q].urlcode);
+            partysocket.to(roomToConnect).emit('start playing', 'video'+roomToConnect+parties[roomToConnect].nowPlaying);
+        }
+        console.log(parties[roomToConnect].filepath);
+    });
     socket.on('actionPerform', (videoStatus) => {
-        currentStatus = videoStatus;
+        parties[roomToConnect].currentStatus = videoStatus;
         console.log('user ' + socket.id + ' changed video status to ' + videoStatus);
-        socket.to(roomToConnect).emit('statusUpdate', currentStatus);
+        socket.to(roomToConnect).emit('statusUpdate', videoStatus);
     });
     socket.on('name set', (nick) => {
         console.log('user ' +socket.id+ ' sets name to ' + nick); //print the chat message event
         parties[roomToConnect].members[socket.id] = nick;
+        partysocket.to(roomToConnect).emit('userPopulate', (Object.values(parties[roomToConnect].members))); //update everyone in the room's userlist
     });
     socket.on('chat sent', (msg) => {
         if (msg.trim().length !== 0) {
@@ -213,6 +266,10 @@ partysocket.on('connection', (socket) => {
     });
     socket.on('disconnect', () => {
         console.log('user disconnected with socket id ' + socket.id);
+        if (parties[roomToConnect]) { //if room still exists..
+            delete parties[roomToConnect].members[socket.id]; //remove from list of users currently here
+            partysocket.to(roomToConnect).emit('userPopulate', (Object.values(parties[roomToConnect].members))); //update everyone in the room's userlist
+        }
     })
 });
 
